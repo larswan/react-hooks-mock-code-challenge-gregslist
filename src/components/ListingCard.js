4da56d24565a3f4d1760c27a8429df09d44bf816
Larson Collier <collierlarson@gmail.com>
2023-03-26 19:23:06 -0400

    first commit

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,24 +1,31 @@
-import React from "react";
-
-function ListingCard() {
+function ListingCard({posts}) {
+ 
+  
   return (
-    <li className="card">
-      <div className="image">
-        <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
-      </div>
-      <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
-        ) : (
-          <button className="emoji-button favorite">☆</button>
-        )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
-      </div>
-    </li>
-  );
-}
+    posts.map((post)=>{
+        
+        return(
+          <li className="card">
+            <div className="image">
+              <span className="price">$0</span>
+              <img src={post.image} alt={"description"} />
+            </div>
+            <div className="details">
+              {true ? (
+                <button className="emoji-button favorite active">★</button>
+              ) : (
+                <button className="emoji-button favorite">☆</button>
+              )}
+              <strong>{"description"}</strong>
+              <span> · {"location"}</span>
+              <button className="emoji-button delete">🗑</button>
+            </div>
+          </li>
+        )
+      }
+    )
+  )
+ }
+
 
 export default ListingCard;
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,14 +1,40 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState, useEffect } from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
-  return (
-    <main>
-      <ul className="cards">
-        {/* use the ListingCard component to display listings */}
-      </ul>
-    </main>
-  );
+  const [posts, setPosts] = useState([])
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(()=>{
+      fetch("http://localhost:6001/listings")
+      .then(req=>req.json())
+      .then(res => {setIsLoaded(true); setPosts(res); console.log(posts) })
+      }, [])
+
+  // return statements
+  {
+
+    // return(
+    //   <div>
+    //     <ListingCard posts={posts} />
+    //   </div>
+    // )
+    if(!isLoaded) {
+      return (
+        <div>Loading...</div>
+      )
+    }
+    else{
+      setIsLoaded(false)
+      return (
+        <main>
+          <ul className="cards">
+            <ListingCard posts={posts} />
+          </ul>
+        </main>
+      )
+    }
+  }
 }
 
 export default ListingsContainer;
